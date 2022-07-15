f644ca4671255cd916433085199b88d4c017426f
gm12180 <lesleymukami@gmail.com>
2022-07-15 12:53:54 -0700

    fix

--- print_alpha.c
+++ print_alpha.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_string - loops through a string and prints
  * every character
@@ -10,13 +9,13 @@
  */
 int print_string(va_list l, flags_t *f)
 {
-    char *s = va_arg(l, char *);
+char *s = va_arg(l, char *);
 
-    (void)f;
+(void)f;
 
-    if (!s)
-        s = "(null)";
-    return (_puts(s));
+if (!s)
+s = "(null)";
+return (_puts(s));
 }
 
 /**
@@ -28,7 +27,7 @@ int print_string(va_list l, flags_t *f)
  */
 int print_char(va_list l, flags_t *f)
 {
-    (void)f;
-    _putchar(va_arg(l, int));
-    return (1);
+(void)f;
+_putchar(va_arg(l, int));
+return (1);
 }
